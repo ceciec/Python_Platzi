09860c5e3715e4ec94de0a3af674244613f365ad
Cecilia <ceciec04@gmail.com>
2024-06-19 18:37:25 -0500

    FastAPI

--- web-server/main.py
+++ web-server/main.py
@@ -1,7 +1,22 @@
 import store
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+@app.get('/')
+def get_list():
+    return [1,2,3,]
+
+@app.get('/contact', response_class=HTMLResponse)
+def get_list():
+    return """
+        <h1>Mi nombre es Cecilia</h1>
+        <p>Me gusta mucho aprender programacion.</p>
+    """
 
 def run():
     store.get_categories()
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     run()
